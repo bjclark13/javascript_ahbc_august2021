9e2b3a25fbd51dcbd651bd6b543536d1b571206d
BJ Clark <bjclark13@outlook.com>
2021-09-29 21:26:22 -0400

    Add solution

--- code-alongs/star-wars/script.js
+++ code-alongs/star-wars/script.js
@@ -2,7 +2,7 @@
 //
 // Call https://swapi.dev/api/films/
 // this will return information about each star wars films
-const films = fetch('https://swapi.dev/api/films/'); // the pager
+const films = fetch('https://swapi.dev/api/films/1'); // the pager
 // console.log(films);
 
 // when the data returns
@@ -10,45 +10,27 @@ films // this is the promise
 	.then(response => {
 		// first then
 		// then means when the API finishes downloading
-		// console.log(response);
+		console.log(response);
 
 		return response.json(); // converts into javascript object with the data requested
 	})
 	.then(data => {
 		// second then, after we get the javascript object
-		// console.log(data);
+		console.log(data);
 
-		const results = data.results;
+		const div = document.createElement('div');
+		const h2 = document.createElement('h2');
 
-		// See documentation on .sort()
-		results.sort((episode1, episode2) => {
-			return episode1.episode_id - episode2.episode_id;
-		});
+		// setting the text of the h2 to the results from the api
+		h2.innerText = data.title;
+		div.append(h2);
 
-		// do something
-		for (let result of results) {
-			console.log(result);
-			// loop through each films
-			// create a DOM element
-			// and add it to our page
+		// adding the opening crawl
+		const crawl = document.createElement('p');
+		crawl.innerText = data.opening_crawl;
+		div.append(crawl);
 
-			// <div> <h2> film title </h2></div>
-			const div = document.createElement('div');
-			const h2 = document.createElement('h2');
-
-			// setting the text of the h2 to the results from the api
-			h2.innerText = result.title;
-			div.append(h2);
-
-			// adding the opening crawl
-			const crawl = document.createElement('p');
-			crawl.innerText = result.opening_crawl;
-			div.append(crawl);
-
-			document.querySelector('main').append(div); // add div to the end of the main
-
-			console.log(result.title);
-		}
+		document.querySelector('main').append(div); // add div to the end of the main
 	});
 
 // same as previous lines
